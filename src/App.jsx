66cd174411b056e40124bd25c8db42fc65194bfb
Abdullah <abdullahmubashir878@gmail.com>
2025-07-22 19:02:21 +0500

    Initial commit - frontend files

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import Login from "./pages/login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointments from "./pages/Admin/AllApointments";
+import AddDoctors from "./pages/Admin/AddDoctors";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import { DoctorContext } from "./context/DoctorContext";
+import DoctorDashboard from "./pages/Doctor/doctorDashboard";
+import DoctorAppointments from "./pages/Doctor/doctorAppointments";
+import DoctorProfile from "./pages/Doctor/doctorProfile";
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
+  return aToken || dToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          {/* Admin Routes */}
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllApointments />} />
+          <Route path="/add-doctor" element={<AddDoctors />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+          {/* Doctor Routes */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointments />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
+      <div>
+        <Login />
+        <ToastContainer />
+      </div>
+    </>
+  );
+};
+
+export default App;
